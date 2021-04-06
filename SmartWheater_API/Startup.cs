271fe270fd632c79dc696753c35e338aa8674df3
Marcus Vinicius Citolino <marcusviniciuscitolino@outlook.com>
2021-04-05 22:29:37 -0300

    Alterando Swagger

--- SmartWheater_API/Startup.cs
+++ SmartWheater_API/Startup.cs
@@ -58,13 +58,16 @@ namespace SmartWheater_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartWheater_API v1"));
             }
-
+            else
+            {
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartWheater_API v1"));
+            }
             app.UseHttpsRedirection();
 
             app.UseRouting();
